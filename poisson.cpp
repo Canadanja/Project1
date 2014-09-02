73e37dd141018ecb123e2aedc66ec311968188e8
Jannes Klee <jannes.klee@gmail.com>
2014-09-02 11:30:19 +0200

    some comments

--- poisson.cpp
+++ poisson.cpp
@@ -13,35 +13,39 @@ void tridiag (double h_step, int n, double * u);
 
 int main()
 {   
-    unsigned int n, i;                              // number of steps        //
-    double x, x_min, x_max;                         // range of variable      // 
-    double *u, u_min, u_max;                        // derichlet boundaries   //
-    double h_step;                                  // stepsize               //
+    unsigned int n, i;                              
+    double x, x_min, x_max;                        
+    double *u, u_min, u_max;                      
+    double h_step;                               
     
     n = 50;                                         // number of steps        //
-    x_min = 0.;                                     //                        //
-    x_max = 1.;
-    x = x_min; 
-    u_min = 0;                                      // derichlet-boundaries   //
-    u_max = 0;
-    h_step = (x_max - x_min) / (n + 1);
+    x_min = 0.;                                     // area of calculation    //
+    x_max = 1.;                                     //                        //
+    x = x_min;                                      // initial value for var. // 
+    u_min = 0;                                      // dirichlet-boundaries   //
+    u_max = 0;                                      //                        //
+    h_step = (x_max - x_min) / (n + 1);             // stepsize               // 
     
-    u = new double[n + 2];
-    u[0] = u_min;
-    u[n+1] = u_max;
+    u = new double[n + 2];                          // initialize array for   //
+    u[0] = u_min;                                   // solution u             //
+    u[n+1] = u_max;                                 //                        // 
     
+    // tridiag solves the equation Au = f
     tridiag (h_step, n, u); 
     
+    // output
     for (i = 0; i <= n + 1; i++)
     {
     cout << u[i] << endl;
     }
+    
     delete [] u;
     return 0;
 }
 
 double func (double x)
 {
+    // second derivative rhs-function 
     double f;
 
     f = 100.*exp(-10.*x);
@@ -49,6 +53,8 @@ double func (double x)
 }
 
 void tridiag (double h_step, int n, double *u){
+    /* Uses the thomas algorithm. Code from "Numerical Recipes,               *  
+     * Third Edition", p. 56 f.                                               */
     double a,b,c;
     double btemp;
     unsigned int i; 
@@ -59,7 +65,7 @@ void tridiag (double h_step, int n, double *u){
     b = 2 ; 
 
     btemp = b;
-    u[i] = func(h_step*1)/btemp;
+    u[1] = func(h_step*1)/btemp;
     for(i = 2 ; i <= n; i++){
         temp[i] = c/btemp;
         btemp = b - a*temp[i];
