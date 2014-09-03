1406082cbd23b5ceefb5a102e78283d695fd77ed
Jannes Klee <jannes.klee@gmail.com>
2014-09-03 15:05:26 +0200

    added: solving tridiagonal matrix for poisson equation

--- poisson.cpp
+++ poisson.cpp
@@ -9,6 +9,7 @@ using namespace arma;
 
 double func (double, double);
 void tridiag (double, int, double *); 
+void tridiag_poisson (double, int, double *); 
 
 
 int main()
@@ -18,12 +19,12 @@ int main()
     double *u, u_min, u_max;                      
     double h_step;                               
     
-    n = 50;                                         // number of steps        //
+    n = 100;                                         // number of steps        //
     x_min = 0.;                                     // area of calculation    //
     x_max = 1.;                                     //                        //
     x = x_min;                                      // initial value for var. // 
-    u_min = 0;                                      // dirichlet-boundaries   //
-    u_max = 0;                                      //                        //
+    u_min = 0.;                                     // dirichlet-boundaries   //
+    u_max = 0.;                                     //                        //
     h_step = (x_max - x_min) / (n + 1);             // stepsize               // 
     
     u = new double[n];                              // initialize array for   //
@@ -31,7 +32,7 @@ int main()
     u[n] = u_max;                                   //                        // 
     
     // tridiag solves the equation Au = f
-    tridiag (h_step, n, u); 
+    tridiag_poisson (h_step, n, u); 
     
     // output
     for (i = 0; i <= n + 1; i++)
@@ -52,8 +53,9 @@ double func (double h_step, double x)
     return f; 
 }
 
+//TODO: Change the call of func and or func itself because of unnecessary FLOPs
 void tridiag (double h_step, int n, double *u){
-    /* Uses the thomas algorithm. Code from "Numerical Recipes,               *  
+    /* Uses the Thomas-Algorithm, code from "Numerical Recipes,               *  
      * Third Edition", p. 56 f.                                               */
     double a,b,c;
     double btemp;
@@ -65,7 +67,7 @@ void tridiag (double h_step, int n, double *u){
     b = 2 ; 
 
     btemp = b;
-    u[1] = func(h_step, h_step*1)/btemp;
+    u[1] = func(h_step, h_step*1.)/btemp;
     for(i = 2 ; i <= n; i++){
         temp[i] = c/btemp;
         btemp = b - a*temp[i];
@@ -75,7 +77,24 @@ void tridiag (double h_step, int n, double *u){
     for(i = n-1; i >= 1; i--){
         u[i] -= temp[i+1]*u[i+1];
     }
-
-
 }
 
+//TODO: Change the call of func and or func itself because of unnecessary FLOPs
+void tridiag_poisson (double h_step, int n, double *u){
+    /* Just usable for poisson equation tridiagonal matrices. Reduces number  *
+     * of FLOPs to 6N.                                                        */
+    double a,b,c;
+    unsigned int i;
+    vec ftemp(n);
+
+    a = -1;
+    b = 2 ;
+
+    for (i = 2; i <= n; i++){
+        ftemp[i] = ftemp[i-1] + func(h_step, h_step*i);
+    }
+
+    for (i = n-1; i >= 1; i--){
+        u[i] = (ftemp[i] + u[i+1]*i)/(1. + i);
+    }
+}
