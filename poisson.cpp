59d18b0d06f5e0c2c5f2b178b95d95d1c657077b
Jannes Klee <jannes.klee@gmail.com>
2014-09-07 21:27:24 +0200

    cleanup & comments

--- poisson.cpp
+++ poisson.cpp
@@ -18,13 +18,12 @@ vec lu_armadillo (double, unsigned int);
 
 int main()
 {   
-    clock_t time_tri, time_lu;
-    unsigned int n, i;                              
-    int j;
-    double x_min, x_max;                        
-    double *u, u_min, u_max;                      
-    double h_step, err;                               
-    vec v;
+    clock_t time_tri, time_lu;                      // calculate time         //
+    unsigned int n, i;                              // number of steps, index //
+    double x_min, x_max;                            // x-boundaries           //
+    double *u, u_min, u_max;                        // num. sol., bound.      //
+    double h_step, err;                             // stepwidth, error       //  
+    vec v;                                          // solution of lu-decomp. //
     
     n = 10;                                         // number of steps        //
     x_min = 0.;                                     // area of calculation    //
@@ -41,17 +40,17 @@ int main()
 
     // tridiag solves the equation Au = f
     // tridiagonal poisson code
-    time_tri = clock();
-    tridiag_poisson (h_step, n, u); 
-    //tridiag (h_step, n, u);
+    time_tri = clock() ;
+    tridiag_poisson (h_step, n, u);                 // own algorithm          //
+    //tridiag (h_step, n, u);                       // thomas algorithm       //
     time_tri = clock() - time_tri;    
 
     // general lu decomposition code
     time_lu = clock();
-    v = lu_armadillo(h_step, n);
+    v = lu_armadillo(h_step, n);                    // lu decomposition       //
     time_lu = clock() - time_lu;
 
-    err = error(h_step, n, u);
+    err = error(h_step, n, u);                      // error-evalution        //
 
     // output
     cout << "--------------------------------------------" << endl;
@@ -109,13 +108,15 @@ void tridiag_poisson (double h_step, unsigned int n, double *u){
 
     for (i = n; i >= 1; i--){
         u[i] = (ftemp[i] + u[i+1]*i)/(1.+i);                // 4 FLOPs
-    }                                                       // 6 FLOPs
+    }                                                       // -> 6 FLOPs
 }
 
 double error (double h_step, unsigned int n, double *u){
+    /* Calculates the logarithmic error bewteen numerical and analytical      *
+     * solution.                                                              */
     unsigned int i;
     double eps, eps_max, u_ana;
-    eps_max = -100.0; // not a nice solution 
+    eps_max = -100.0; 
 
     for (i = 1; i <= n; i++){
         u_ana = solution(h_step*i);
@@ -138,6 +139,7 @@ double func (double h_step, double x)
 
 double solution (double x)
 {
+    // analytical solution 
     double f;
     f = 1.-(1.-exp(-10.))*x-exp(-10.*x);
     return f; 
@@ -145,14 +147,17 @@ double solution (double x)
 
 vec lu_armadillo(double h_step, unsigned int n)
 {
+    /* lu-decomposition to solve the same Equation than the tridiag           *
+     * algorithms but width a worse performance                               */
     unsigned int i;
 
-    vec f(n);
-    mat A(n,n);
-    vec y, x; 
-    mat L, U, P;
+    vec f(n);                                       // Ax = f                 //
+    mat A(n,n);                                     //                        // 
+    vec y, x;                                       // Ax = LUx = Ly = f      //
+    mat L, U, P;                                    //                        //
     
 
+    // define matrix A for as in poisson equation
     f(0) = func(h_step, h_step*(1));
     A(0,0) = 2.0;
     for (i = 1; i < n; i++){
@@ -162,8 +167,8 @@ vec lu_armadillo(double h_step, unsigned int n)
         f(i) = func(h_step, h_step*(i+1));
     }
 
-    lu(L, U, A);
-    solve(y, L, f);
-    solve(x, U, y); 
+    lu(L, U, A);                                    // lu-decomposition       //
+    solve(y, L, f);                                 // solve Ly = f           //
+    solve(x, U, y);                                 // solve Ux = y           //
     return x; 
 }
